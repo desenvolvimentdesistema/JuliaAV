f4173dbf1d9cb13690469e3ec823e0ed5687c35d
JúliaAV <juaugustiveiga@gmail.com>
2024-03-27 09:50:04 -0300

    making progress

---
+++ JuliaAV/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JuliaAV;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
