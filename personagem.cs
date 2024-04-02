d6c32de55fae6f220b895f56e83647b7ef51766f
Júlia <juaugustiveiga@gmail.com>
2024-04-02 09:03:59 -0300

    making progress

---
+++ personagem.cs
@@ -0,0 +1,22 @@
+public class personagem
+{
+    protected string arquivo;
+    proteced double fome;
+    public string get arquivo
+    {
+     return arquivo;
+    }
+    public double getfome()
+    {
+
+     }    return fome;
+    }
+
+public class cat : personagem
+{
+        public cat()
+        {
+        arquivo=blackcatwithyelloweyes.jpg
+        fome=0.5;
+        }
+}
